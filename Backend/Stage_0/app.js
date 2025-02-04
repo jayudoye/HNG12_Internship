2b04da64aaa4fe65e617da6ce7a38c6331dd404f
jayudoye <joeludoye@gmail.com>
2025-02-04 20:53:52 +0100

    Public API

--- Backend/Stage_0/app.js
+++ Backend/Stage_0/app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 //Email, Current Datetime and Github URL
-const email = process.env.EMAIL
-const current_datetime = new Date().toISOString()
-const github_url = process.env.GITHUB_URL
+const email = process.env.EMAIL;
+const current_datetime = new Date().toISOString();
+const github_url = process.env.GITHUB_URL;
 
 //Enabling CORS for all Origin
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
 
@@ -18,10 +18,10 @@ app.get('/', (req, res) => {
         email,
         current_datetime,
         github_url
-    }
-  res.json(response)
-})
+    };
+  res.json(response);
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
