ed8093420ed23a36a6a82119366b17cb6e7aec4c
jayudoye <joeludoye@gmail.com>
2025-02-04 22:38:17 +0100

    Public API

--- Backend/Stage_0/app.js
+++ Backend/Stage_0/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 //Email, Current Datetime and Github URL
-const email = process.env.EMAIL;
+const email = process.env.EMAIL || 'joeludoye@gmail.com';
 const current_datetime = new Date().toISOString();
-const github_url = process.env.GITHUB_URL;
+const github_url = process.env.GITHUB_URL || 'https://github.com/jayudoye/HNG12_Internship';
 
 //Enabling CORS for all Origin
 app.use(cors());
